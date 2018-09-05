b69a8f0ab4c55cfe0fa97b31d8ceed099b6e9843
Chris <chrispackett1998@gmail.com>
2018-09-05 15:51:26 -0400

    Fetched movies from API. Passed as props to Movies component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
     <Navbar />
+    <Movies :movies="movies"/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/layout/Navbar.vue'
+import Movies from './components/movies/Movies.vue'
 
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    Movies
+  },
+  data: function () {
+    return {
+      movies: []
+    }
+  },
+  mounted: function () {
+    const API_KEY = '39df1c4c7a287510f53854893ba3d788'
+    let URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(movieData => {
+      this.movies = movieData.results
+    })
   }
 }
 </script>
---
+++ src/components/movies/Movies.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "movies",
+    props: {
+        movies: Array
+    }
+}
+</script>
