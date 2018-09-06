3301e7fda2647fe348a657b5a77e907f621e6971
Chris <chrispackett1998@gmail.com>
2018-09-06 01:27:17 -0400

    added MovieDetails content

--- src/components/movies/MovieDetails.vue
+++ src/components/movies/MovieDetails.vue
@@ -1,8 +1,28 @@
 <template>
-<div v-bind="getMovieFromArray">
+<div v-bind="getMovieFromArray" class="col-md-12">
     <router-link :to="'/'">
         <button class="btn btn-dark btn-sm mb-4">Go Back</button>
     </router-link>
+
+    <div class="card">
+        <div class="card-header">
+            <strong>{{ movie.title }}</strong>
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                {{ movie.overview }}
+            </p>
+        </div>
+    </div>
+
+    <ul class="list-group mt-3">
+        <li class="list-group-item">
+            <strong>Adult Film</strong>: {{ isAdultMovie(movie.adult) }}
+        </li>
+        <li class="list-group-item">
+            <strong>Release Date</strong>: {{ movie.release_date }}
+        </li>
+    </ul>
 </div>
 </template>
 
@@ -21,6 +41,11 @@ export default {
     computed: {
         getMovieFromArray: function () {
             this.movie = this.movies.filter(movie => movie.id == this.id)[0]
+        }
+    },
+    methods: {
+        isAdultMovie: function (isAdult) {
+            return isAdult ? 'Yes' : 'No'
         }
     }
 }
