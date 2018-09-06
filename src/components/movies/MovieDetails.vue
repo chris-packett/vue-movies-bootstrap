79445b5db4148818762f160d6e03c754cc46e0ab
Chris <chrispackett1998@gmail.com>
2018-09-06 13:28:50 -0400

    fixed side effects in MovieDetails

--- src/components/movies/MovieDetails.vue
+++ src/components/movies/MovieDetails.vue
@@ -1,26 +1,26 @@
 <template>
-<div v-bind="getMovieFromArray" class="col-md-12">
+<div class="col-md-12">
     <router-link :to="'/'">
         <button class="btn btn-dark btn-sm mb-4">Go Back</button>
     </router-link>
 
     <div class="card">
         <div class="card-header">
-            <strong>{{ movie.title }}</strong>
+            <strong>{{ currentMovie.title }}</strong>
         </div>
         <div class="card-body">
             <p class="card-text">
-                {{ movie.overview }}
+                {{ currentMovie.overview }}
             </p>
         </div>
     </div>
 
     <ul class="list-group mt-3">
         <li class="list-group-item">
-            <strong>Adult Film</strong>: {{ isAdultMovie(movie.adult) }}
+            <strong>Adult Film</strong>: {{ isAdultMovie }}
         </li>
         <li class="list-group-item">
-            <strong>Release Date</strong>: <span>{{ movie.release_date | moment("dddd, MMMM Do YYYY") }}</span>
+            <strong>Release Date</strong>: <span>{{ currentMovie.release_date | moment("dddd, MMMM Do YYYY") }}</span>
         </li>
     </ul>
 </div>
@@ -32,20 +32,18 @@ export default {
     data: function () {
         return {
             id: this.$route.params.id,
-            movie: {}
+            movie:{}
         }
     },
     props: {
         movies: Array
     },
     computed: {
-        getMovieFromArray: function () {
-            this.movie = this.movies.filter(movie => movie.id == this.id)[0]
-        }
-    },
-    methods: {
-        isAdultMovie: function (isAdult) {
-            return isAdult ? 'Yes' : 'No'
+        currentMovie : function () {
+            return this.movies.filter(movie => movie.id == this.id)[0]
+        }, 
+        isAdultMovie: function () {
+             return this.currentMovie.adult ? 'Yes' : 'No'
         }
     }
 }
