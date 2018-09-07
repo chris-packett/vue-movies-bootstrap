c3351c0dd095eaa210075756d36140e5ff1e21e6
Chris Packett <chrispackett1998@gmail.com>
2018-09-07 00:53:00 -0400

    added scrollBehavior to router

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ Vue.use(require('vue-moment'))
 
 const router = new VueRouter({
   mode: 'history',
-  routes: routes
+  routes: routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
 
 Vue.config.productionTip = false
